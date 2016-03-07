9483cdeed3ad82ed4af56e2d3520ccdc649027a0
Michael Rico <rico.michael@gmail.com>
2016-03-07 12:03:54 -0800

    Implement Bee constructor, inherits from Grub

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
 var Bee = function() {
+  Grub.call(this);  // this gives the bee its unique properties
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+
+Bee.prototype.constructor = Bee;
